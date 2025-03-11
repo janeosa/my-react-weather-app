e753f9ef26c4f48f2d5029dc5d49200042c8b0d6
miss-jane-osa <jane_i@hotmail.co.uk>
2025-03-11 18:44:02 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/janeosa"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jane Ighodaro
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/janeosa/my-react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://my-react-weather-app-by-jane-osa.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/janeosa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jane Ighodaro
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/janeosa/my-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://my-react-weather-app-by-jane-osa.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
