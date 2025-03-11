1541091a861f2bba55cfe3a08913aaf254ee3c34
miss-jane-osa <jane_i@hotmail.co.uk>
2025-03-11 18:10:27 +0000

    Added Basic Footer with URL links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/janeosa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jane Ighodaro
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/janeosa/my-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://my-react-weather-app-by-jane-osa.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
