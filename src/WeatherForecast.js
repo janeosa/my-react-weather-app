9193baae26a54fea831c003de12fcd28efa3e9e0
miss-jane-osa <jane_i@hotmail.co.uk>
2025-03-18 01:14:54 +0000

    Set up component to display real Forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  let apiKey = "10tc88b5de8d7eoa39c8f2ea84faadb2";
-  let city = props.data.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <div className="WeatherForecast-icon">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              width="58"
-              height="58"
-              alt="insert description"
-              className="WeatherForecast-icon"
-            />
-          </div>{" "}
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">19°</span>
-            <span className="WeatherForecast-temperature-min">10°</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "10tc88b5de8d7eoa39c8f2ea84faadb2";
+    let city = props.data.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
