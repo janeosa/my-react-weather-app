6c900680c3fad0d1df4b5b0a9361958321dbc82a
miss-jane-osa <jane_i@hotmail.co.uk>
2025-03-18 00:09:53 +0000

    Installing the forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -56,7 +56,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast data={weatherData} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "10tc88b5de8d7eoa39c8f2ea84faadb2";
+  let city = props.data.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
